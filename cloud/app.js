28edf9a49e1a6d5be22472f0b0850007ee903763
yunian <286571362@qq.com>
2015-08-07 17:29:01 +0800

    express

--- cloud/app.js
+++ cloud/app.js
@@ -1,15 +1,23 @@
 // 在 Cloud code 里初始化 Express 框架
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
 var ejs = require('ejs');
 
-// App 全局配置
-app.engine('.html', ejs.__express);
-app.set('view engine', 'html');// app.set('view engine', 'ejs');
-app.set('views','cloud/views');   // 设置模板目录
-//app.set('view engine', 'ejs');
-// 设置 template 引擎
-app.use(express.bodyParser());    // 读取请求 body 的中间件
+// 设置 view 引擎
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.bodyParser());
+
+app.get('/', function(req, res) {
+    res.render('index', { currentTime: new Date() });
+});
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
